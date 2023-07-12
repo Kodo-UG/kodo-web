acc5d09ade5fccdf82736c1a78362a897f87fd3b
Bkroland19 <bkroland19@gmail.com>
2023-07-12 17:22:48 +0300

    register

--- src/App.js
+++ src/App.js
@@ -24,6 +24,7 @@ import RootApplication from "./pages/Dashboard/applications";
 import StepperElementFour from "./pages/Register/Scholarships/StepperElementFour";
 import StepperElementSix from "./pages/Register/Scholarships/StepperElementSix";
 import StepperElement7 from "./pages/Register/Scholarships/StepperElement7";
+import StepElement8 from "./pages/Register/Scholarships/StepElement8";
 const App = () => {
   return (
     <Switch>
@@ -35,6 +36,7 @@ const App = () => {
       <PrivateRoute exact path="/applications" component={RootApplication} />
 
       <Route exact path="/profile" component={MainProfile} />
+      <Route exact path="/final" component={StepElement8} />
 			<Route exact path="/test3" component={StepperElement7} />
 			<Route  exact path="/test2" component={StepperElementSix} />
       <Route exact path="/stepper" component={Stepper} />
--- src/pages/Register/Scholarships/StepperElement7.jsx
+++ src/pages/Register/Scholarships/StepperElement7.jsx
@@ -1,13 +1,16 @@
+/* eslint-disable no-script-url */
 import React from "react";
 import "./stepperElement.css";
 import { useDispatch, useSelector } from "react-redux";
 import { updateFormData } from "../../../toolkit/formReducer";
+import { useHistory } from "react-router-dom";
 
 import { Link } from "react-router-dom";
 
 function StepperElement7() {
 	const dispatch = useDispatch();
 	const formData = useSelector((state) => state.formData);
+	const history = useHistory();
 
 	const handleCountryChange = (e) => {
 		const { name, value } = e.target;
@@ -20,6 +23,10 @@ function StepperElement7() {
 		console.log(`Field: ${name}, Value: ${value}`);
 		// Dispatch an action to update the form data in the Redux store
 		dispatch(updateFormData({ field: "city", value: value }));
+	};
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
 	};
 
 	return (
@@ -103,7 +110,7 @@ function StepperElement7() {
 										Where do you currently live?
 									</h2>
 								</span>
-								<form>
+								<form onSubmit={handleSubmit}>
 									<div>
 										<div className="_fieldGroup_1g3ja_1">
 											<input
@@ -130,27 +137,29 @@ function StepperElement7() {
 										</div>
 									</div>
 									<div className="_pageActions_pmptr_26">
-										<button
-											type="submit"
-											className="_buttonContinue_pmptr_46 _button_pmptr_30"
-											data-testid="continue"
-										>
-											<span>Continue</span>
-											<svg
-												width="26"
-												height="16"
-												viewBox="0 0 26 16"
-												fill="none"
-												xmlns="http://www.w3.org/2000/svg"
+										<Link to="/final">
+											<button
+												type="submit"
+												className="_buttonContinue_pmptr_46 _button_pmptr_30"
+												data-testid="continue"
 											>
-												<path
-													fillRule="evenodd"
-													clipRule="evenodd"
-													d="M16.6774 0.468629C17.3023 -0.15621 18.3153 -0.15621 18.9402 0.468629L25.3402 6.86863C25.965 7.49347 25.965 8.50653 25.3402 9.13137L18.9402 15.5314C18.3153 16.1562 17.3023 16.1562 16.6774 15.5314C16.0526 14.9065 16.0526 13.8935 16.6774 13.2686L20.346 9.6H1.80879C0.925131 9.6 0.208786 8.88366 0.208786 8C0.208786 7.11634 0.925131 6.4 1.80879 6.4H20.346L16.6774 2.73137C16.0526 2.10653 16.0526 1.09347 16.6774 0.468629Z"
-													fill="white"
-												></path>
-											</svg>
-										</button>
+												<span>Continue</span>
+												<svg
+													width="26"
+													height="16"
+													viewBox="0 0 26 16"
+													fill="none"
+													xmlns="http://www.w3.org/2000/svg"
+												>
+													<path
+														fillRule="evenodd"
+														clipRule="evenodd"
+														d="M16.6774 0.468629C17.3023 -0.15621 18.3153 -0.15621 18.9402 0.468629L25.3402 6.86863C25.965 7.49347 25.965 8.50653 25.3402 9.13137L18.9402 15.5314C18.3153 16.1562 17.3023 16.1562 16.6774 15.5314C16.0526 14.9065 16.0526 13.8935 16.6774 13.2686L20.346 9.6H1.80879C0.925131 9.6 0.208786 8.88366 0.208786 8C0.208786 7.11634 0.925131 6.4 1.80879 6.4H20.346L16.6774 2.73137C16.0526 2.10653 16.0526 1.09347 16.6774 0.468629Z"
+														fill="white"
+													></path>
+												</svg>
+											</button>
+										</Link>
 									</div>
 								</form>
 								<div className="_pageActions_pmptr_26"></div>
