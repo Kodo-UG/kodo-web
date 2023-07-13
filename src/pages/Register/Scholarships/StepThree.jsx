d405851fc298e3c21cecb591ad11467c640817b5
WILTON-DEV <balukuwilton@gmail.com>
2023-07-13 23:36:28 +0300

    cln

--- src/pages/Register/Scholarships/StepThree.jsx
+++ src/pages/Register/Scholarships/StepThree.jsx
@@ -1,146 +1,178 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable no-unused-vars */
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { updateFormData } from "../../../toolkit/formReducer";
 import "./stp3.css";
+import { Link } from "react-router-dom";
 import { Select } from "antd";
+import { AiOutlineDown } from "react-icons/ai";
+import { MdOutlineKeyboardArrowUp } from "react-icons/md";
+import { useHistory } from "react-router-dom";
 const { Option } = Select;
 
 const StepThree = ({ nextStep, prevStep, formData, setFormData }) => {
-	const { scholarshipcategory } = useSelector((state) => state.formData);
-	const [data, setData] = useState();
-	const [selectedOption, setSelectedOption] = useState("");
-	const [dropdownOpen, setDropdownOpen] = useState(false); // State to track the dropdown open/close state
+  const { scholarshipcategory } = useSelector((state) => state.formData);
+  const [data, setData] = useState();
+  const [selectedOption, setSelectedOption] = useState("select a subject");
+  const [dropdownOpen, setDropdownOpen] = useState(false); // State to track the dropdown open/close state
 
-	const dispatch = useDispatch();
+  const history = useHistory();
+  const dispatch = useDispatch();
 
-	useEffect(() => {
-		fetchData();
-	}, [scholarshipcategory]);
+  const fetchData = async () => {
+    try {
+      const { data } = await axios.get(
+        `https://demo.kodoscholarships.com/api/v1/scholarship/categories/${scholarshipcategory}`
+      );
+      setData(data.data.categories);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-	const fetchData = async () => {
-		try {
-			const { data } = await axios.get(
-				`https://demo.kodoscholarships.com/api/v1/scholarship/categories/${scholarshipcategory}`
-			);
-			setData(data.data.categories);
-		} catch (error) {
-			console.log(error);
-		}
-	};
+  useEffect(() => {
+    fetchData();
+  }, [fetchData, scholarshipcategory]);
 
-	console.log(selectedOption);
+  const selectStyle = {
+    border: "none",
+    boxShadow: "none",
+    width: "100%",
+  };
+  const handleChange = (value) => {
+    dispatch(updateFormData({ field: "subject", value: value }));
+  };
 
-	const handleChange = (value) => {
-		dispatch(updateFormData({ field: "subject", value: value }));
-	};
+  const handleList = () => {
+    setDropdownOpen(!dropdownOpen);
+  };
+  const handleOptionChange = (selected) => {
+    setSelectedOption(selected);
+    dispatch(updateFormData({ field: "subject", value: selected }));
+  };
+  return (
+    <div
+      className="selections-contai "
+      style={{
+        transform: "none",
+        transition: "transform 225ms cubic-bezier(0, 0, 0.2, 1) 0ms",
+        width: "40vw",
+      }}
+    >
+      <h2
+        style={{
+          paddingTop: "0px",
+          paddingBottom: "0px",
+          color: "#00234a",
+          fontFamily: "Lexend Deca, Helvetica, Arial, sans-seri",
+        }}
+      >
+        What subject do you want to study?
+      </h2>
+      <div className="dropdown-containe bg-secondary">
+        <button
+          className="dropdow "
+          style={{
+            backgroundColor: "#fff",
+            border: "1px solid #e9e9e9",
 
-	return (
-		<div className="">
-			<div className="app-wrapper">
-				<div style={{ padding: "2rem" }} className="">
-					<h2 style={{ fontSize: "2rem" }}>
-						What subject do you want to study?
-					</h2>
-					<div className="dropdown-container">
-  <button className="dropdown">
-    <div className="dropdown-top">
-      <div className="select-title" onClick={nextStep}>Select a subject</div>
-      <div className="arrow">
-        <div className="down"></div>
+            padding: "20px 10px",
+            textTransform: "none",
+            width: "100%",
+          }}
+          onClick={() => setDropdownOpen(!dropdownOpen)}
+        >
+          <div
+            className=""
+            style={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "space-between",
+              margin: "auto",
+              width: "90%",
+            }}
+          >
+            <div
+              className=""
+              style={{
+                color: "#424242",
+                fontSize: "16px",
+                fontWeight: "500",
+                height: "28px",
+                letterSpacing: "0",
+                lineHeight: "28px",
+              }}
+            >
+              <p>{selectedOption}</p>
+            </div>
+            <div className="icon" onClick={handleList}>
+              {dropdownOpen ? (
+                <AiOutlineDown size={20} color="red" />
+              ) : (
+                <MdOutlineKeyboardArrowUp size={20} color="red" />
+              )}
+            </div>
+          </div>
+
+          <div
+            className="dropdown-break-line"
+            style={{
+              backgroundColor: "#c11b94",
+              height: "2px",
+              margin: "auto",
+              width: "90%",
+            }}
+          ></div>
+        </button>
+        {dropdownOpen ? (
+          <div
+            id="dropdown-lst"
+            className="dropdown-lis"
+            style={{
+              background: "white", // Glass morphism background color
+              boxShadow: "0 2px 2px 0 rgba(31, 38, 135, 0.37)", // Glass morphism shadow
+              backdropFilter: "blur(5px)", // Glass morphism blur effect
+              padding: "20px", // Padding around the div
+            }}
+          >
+            {data?.map((option, index) => (
+              <button
+                onClick={() => {
+                  handleOptionChange(option);
+                }}
+                type="button"
+                className="option-button"
+                style={{
+                  border: "none",
+                  borderRadius: "2rem",
+                  // fontSize: "40px",
+                  display: "block",
+                  // fontSize: "16px",
+                  fontWeight: "500",
+                  letterSpacing: "0",
+                  // lineHeight: "28px",
+                  padding: "5px",
+                  textAlign: "left",
+                  textTransform: "capitalize",
+                  width: "100%",
+                }}
+              >
+                <h4
+                  style={{ color: "#00234a", borderRadius: "2rem" }}
+                  className="text"
+                  // onClick={history.push("/test")}
+                >
+                  <Link to="/test">{option}</Link>
+                </h4>
+              </button>
+            ))}
+          </div>
+        ) : null}
       </div>
     </div>
-    <div className="dropdown-break-line"></div>
-  </button>
-  <div
-    id="dropdown-list"
-    className="dropdown-list"
-    style={{
-      visibility: "hidden",
-      transform: "translateY(915px) translateY(-10.9727px)",
-    }}
-  >
-    <button type="button" className="option-button">
-      Art &amp; Art History
-    </button>
-    <button type="button" className="option-button">
-      Graphic Design
-    </button>
-    <button type="button" className="option-button">
-      Multimedia Design
-    </button>
-    <button type="button" className="option-button">
-      Photography
-    </button>
-  </div>
-</div>
-
-					{/* <div className="">
-						<Select
-							showSearch
-							className="selectr"
-							placeholder="Search to Select"
-							optionFilterProp="children"
-							onChange={handleChange}
-						>
-							{data?.map((option, index) => (
-								<Option key={index} value={option}>
-									{option}
-								</Option>
-							))}
-						</Select>
-					</div> */}
-				</div>
-			</div>
-			{/* <div
-				className="container"
-				style={{
-					display: "flex",
-					justifyContent: "space-between",
-					width: "50%"
-				}}
-			>
-				<button
-					className="select-option sonic-btn"
-					style={{
-						padding: "10px 20px",
-						border: "none",
-						outline: "none",
-						transition:
-							"background-color 0.3s ease-in-out, color 0.3s ease-in-out",
-						fontFamily: "Arial, sans-serif",
-						fontSize: "16px",
-						lineHeight: "1.5",
-						color: "#fff",
-						cursor: "pointer",
-						marginBottom: "4px"
-					}}
-					onClick={prevStep}
-				>
-					Previous
-				</button>
-				<button
-					className="select-option sonic-btn"
-					style={{
-						padding: "10px 20px",
-						border: "none",
-						outline: "none",
-						transition:
-							"background-color 0.3s ease-in-out, color 0.3s ease-in-out",
-						fontFamily: "Arial, sans-serif",
-						fontSize: "16px",
-						lineHeight: "1.5",
-						color: "#fff",
-						cursor: "pointer",
-						marginBottom: "4px"
-					}}
-					onClick={nextStep}
-				>
-					Next
-				</button>
-			</div> */}
-		</div>
-	);
+  );
 };
 
 export default StepThree;
