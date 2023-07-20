34cb2575917e9014eb275a49e25e5e634d05d56a
WILTON-DEV <balukuwilton@gmail.com>
2023-07-20 13:50:39 +0300

    clean

--- src/pages/Dashboard/scholarship/Scholarship.jsx
+++ src/pages/Dashboard/scholarship/Scholarship.jsx
@@ -11,230 +11,230 @@ import { SiFiles } from "react-icons/si";
 import { useHistory } from "react-router-dom";
 
 const Scholarship = () => {
-	const [data, setData] = useState([]);
-	const [subscription, setSubscription] = useState(false);
-	const [loading, setLoading] = useState(false);
-	const [count, setCount] = useState(1);
-	const [totalPages, setTotalPages] = useState(0);
+  const [data, setData] = useState([]);
+  const [subscription, setSubscription] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [count, setCount] = useState(1);
+  const [totalPages, setTotalPages] = useState(0);
 
-	const history = useHistory();
+  const history = useHistory();
 
-	const isSm = useMediaQuery("only screen and (max-width : 700px)");
-	const isMd = useMediaQuery(
-		"only screen and (min-width : 700px) and (max-width : 1250px)"
-	);
-	const isLg = useMediaQuery(
-		"only screen and (min-width : 1250px) and (max-width : 1300px)"
-	);
-	const isXl = useMediaQuery("only screen and (min-width : 1201px)");
+  const isSm = useMediaQuery("only screen and (max-width : 700px)");
+  const isMd = useMediaQuery(
+    "only screen and (min-width : 700px) and (max-width : 1250px)"
+  );
+  const isLg = useMediaQuery(
+    "only screen and (min-width : 1250px) and (max-width : 1300px)"
+  );
+  const isXl = useMediaQuery("only screen and (min-width : 1201px)");
 
-	const getScholarship = async () => {
-		setLoading(true);
-		try {
-			let res = await axiosInstance.get(
-				"https://demo.kodoscholarships.com/api/v1/scholarship"
-			);
+  const getScholarship = async () => {
+    setLoading(true);
+    try {
+      let res = await axiosInstance.get(
+        "https://demo.kodoscholarships.com/api/v1/scholarship"
+      );
 
-			setData(res.data.data);
-			setSubscription(res.data.subscription);
-		} catch (error) {
-			// Handle   error here
-			throw error;
-		} finally {
-			setLoading(false);
-		}
-	};
+      setData(res.data.data);
+      setSubscription(res.data.subscription);
+    } catch (error) {
+      // Handle   error here
+      throw error;
+    } finally {
+      setLoading(false);
+    }
+  };
 
-	useEffect(() => {
-		getScholarship();
-	}, []);
+  useEffect(() => {
+    getScholarship();
+  }, []);
 
-	let handleFetchMore = async () => {
-		setLoading(true);
-		setCount(count + 1);
-		try {
-			const token = localStorage.getItem("token");
-			const headers = {
-				Authorization: `Bearer ${token}`
-			};
+  let handleFetchMore = async () => {
+    setLoading(true);
+    setCount(count + 1);
+    try {
+      const token = localStorage.getItem("token");
+      const headers = {
+        Authorization: `Bearer ${token}`,
+      };
 
-			let res = await axios.get(
-				`https://demo.kodoscholarships.com/api/v1/scholarship?page=${count}`,
-				{
-					headers
-				}
-			);
+      let res = await axios.get(
+        `https://demo.kodoscholarships.com/api/v1/scholarship?page=${count}`,
+        {
+          headers,
+        }
+      );
 
-			setTotalPages(res.data.totalPages);
+      setTotalPages(res.data.totalPages);
 
-			// setData((prev) => [...prev, res.data.data]);
-			setData(res.data.data);
-			setSubscription(res.data.subscription);
-		} catch (error) {
-			// Handle   error here
-			throw error;
-		} finally {
-			setLoading(false);
-		}
-	};
+      // setData((prev) => [...prev, res.data.data]);
+      setData(res.data.data);
+      setSubscription(res.data.subscription);
+    } catch (error) {
+      // Handle   error here
+      throw error;
+    } finally {
+      setLoading(false);
+    }
+  };
 
-	return (
-		<div>
-			<div className="content-wrapper">
-				<section style={{ marginBottom: "3rem" }} className="content">
-					<div
-						style={{
-							display: "flex ",
-							flexDirection: "column",
-							width: "100%",
-							alignItems: "center",
-							padding: "2px",
-							justifyContent: "center"
-						}}
-					>
-						<div
-							onClick={() => history.push("/profile")}
-							style={{
-								// height: "8rem",
-								width: isSm ? "100%" : "" | isMd ? "50%" : "30%",
-								background: "white",
-								display: "flex",
-								marginTop: "4rem",
-								boxShadow: "2px 2px 4px rgba(0, 0, 0, 0.2)",
-								padding: "1rem",
-								alignItems: "center",
-								cursor: "pointer",
-								borderRadius: "4px"
+  return (
+    <div>
+      <div className="content-wrapper">
+        <section style={{ marginBottom: "3rem" }} className="content">
+          <div
+            style={{
+              display: "flex ",
+              flexDirection: "column",
+              width: "100%",
+              alignItems: "center",
+              padding: "2px",
+              justifyContent: "center",
+            }}
+          >
+            <div
+              onClick={() => history.push("/profile")}
+              style={{
+                // height: "8rem",
+                width: isSm ? "100%" : "" | isMd ? "50%" : "35%",
+                background: "white",
+                display: "flex",
+                marginTop: "4rem",
+                boxShadow: "2px 2px 4px rgba(0, 0, 0, 0.2)",
+                padding: "1rem",
+                alignItems: "center",
+                cursor: "pointer",
+                borderRadius: "4px",
 
-								// justifyContent: "space-between"
-							}}
-						>
-							<div
-								style={{
-									width: "20%",
-									display: "flex",
-									alignItems: "center",
-									justifyContent: "center",
-									marginRight: "1rem"
-								}}
-							>
-								<SiFiles
-									size={75}
-									style={{
-										color: "#1d2755"
-									}}
-								/>
-							</div>
-							<div
-								style={{
-									// height: "100%",
-									width: "100vw"
-								}}
-							>
-								<p
-									style={{
-										fontFamily: "Poppins",
-										fontSize: isSm ? "22px" : "24px",
-										color: "#1c2755",
-										fontWeight: "bold",
-										letterSpacing: "1px"
-									}}
-								>
-									$14,500 In Matches
-								</p>
-								<p
-									style={{
-										fontFamily: "Poppins",
-										fontSize: isSm ? "15px" : "16px",
-										display: "flex",
-										alignItems: "center",
-										lineHeight: "1px",
-										fontWeight: 500,
-										textAlign: "justify",
-										color: "#000",
-										justifyContent: "space-between"
-									}}
-								>
-									Update your profile to match to more
-									<MdArrowForwardIos />
-								</p>{" "}
-								<p
-									style={{
-										fontFamily: "Poppins",
-										fontSize: isSm ? "16px" : "16px",
-										fontWeight: 500,
-										color: "#000"
-									}}
-								>
-									scholarships.
-								</p>
-							</div>
-						</div>
+                // justifyContent: "space-between"
+              }}
+            >
+              <div
+                style={{
+                  width: "20%",
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "center",
+                  marginRight: "1rem",
+                }}
+              >
+                <SiFiles
+                  size={75}
+                  style={{
+                    color: "#1d2755",
+                  }}
+                />
+              </div>
+              <div
+                style={{
+                  // height: "100%",
+                  width: "100vw",
+                }}
+              >
+                <p
+                  style={{
+                    fontFamily: "Poppins",
+                    fontSize: isSm ? "22px" : "24px",
+                    color: "#1c2755",
+                    fontWeight: "bold",
+                    letterSpacing: "1px",
+                  }}
+                >
+                  $14,500 In Matches
+                </p>
+                <p
+                  style={{
+                    fontFamily: "Poppins",
+                    fontSize: isSm ? "15px" : "16px",
+                    display: "flex",
+                    alignItems: "center",
+                    lineHeight: "1px",
+                    fontWeight: 500,
+                    textAlign: "justify",
+                    color: "#000",
+                    justifyContent: "space-between",
+                  }}
+                >
+                  Update your profile to match to more
+                  <MdArrowForwardIos />
+                </p>{" "}
+                <p
+                  style={{
+                    fontFamily: "Poppins",
+                    fontSize: isSm ? "16px" : "16px",
+                    fontWeight: 500,
+                    color: "#000",
+                  }}
+                >
+                  scholarships.
+                </p>
+              </div>
+            </div>
 
-						{/* end of card */}
-						<div
-							style={{
-								fontWeight: 300,
-								fontSize: "30px",
-								margin: "5px",
-								padding: "10px",
-								marginTop: "3rem",
-								fontFamily: "Poppins",
-								lineHeight: "45px"
-							}}
-						>
-							{data.length < 0 ? (
-								<div
-									style={{
-										letterSpacing: "2px",
-										color: "rgb(74,74,74)"
-									}}
-								>
-									No Available scholarships for this program
-								</div>
-							) : (
-								<div
-									style={{
-										letterSpacing: "2px",
-										color: "rgb(74,74,74)"
-									}}
-								>
-									Latest Scholarships
-								</div>
-							)}
-						</div>
-						{/* <DashboardCard>
+            {/* end of card */}
+            <div
+              style={{
+                fontWeight: 300,
+                fontSize: "30px",
+                margin: "5px",
+                padding: "10px",
+                marginTop: "3rem",
+                fontFamily: "Poppins",
+                lineHeight: "45px",
+              }}
+            >
+              {data.length < 0 ? (
+                <div
+                  style={{
+                    letterSpacing: "2px",
+                    color: "rgb(74,74,74)",
+                  }}
+                >
+                  No Available scholarships for this program
+                </div>
+              ) : (
+                <div
+                  style={{
+                    letterSpacing: "2px",
+                    color: "rgb(74,74,74)",
+                  }}
+                >
+                  Latest Scholarships
+                </div>
+              )}
+            </div>
+            {/* <DashboardCard>
 							
 						</DashboardCard> */}
-						{isSm ? <CombinedScholarshipCard /> : <MapCardData />}
-					</div>
-					<div
-						style={{
-							display: "flex",
-							justifyContent: "center",
-							alignItems: "center",
-							padding: "1rem"
-						}}
-					>
-						{count < totalPages && (
-							<Button
-								style={{
-									width: "12rem",
-									backgroundColor: "#1d2755",
-									color: "#fff",
-									fontWeight: "bold",
-									marginBottom: "3rem"
-								}}
-								onClick={() => handleFetchMore()}
-							>
-								{loading ? "loading...." : "Load More"}
-							</Button>
-						)}
-					</div>
-				</section>
-			</div>
-		</div>
-	);
+            {isSm ? <CombinedScholarshipCard /> : <MapCardData />}
+          </div>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              padding: "1rem",
+            }}
+          >
+            {count < totalPages && (
+              <Button
+                style={{
+                  width: "12rem",
+                  backgroundColor: "#1d2755",
+                  color: "#fff",
+                  fontWeight: "bold",
+                  marginBottom: "3rem",
+                }}
+                onClick={() => handleFetchMore()}
+              >
+                {loading ? "loading...." : "Load More"}
+              </Button>
+            )}
+          </div>
+        </section>
+      </div>
+    </div>
+  );
 };
 
 export default Scholarship;
--- src/pages/favorite/index.jsx
+++ src/pages/favorite/index.jsx
@@ -12,142 +12,140 @@ import LargeCard from "../../components/card/LargeCard";
 import LargeCardFavourite from "../../components/card/LargeCardFavourite";
 
 const Favorite = () => {
-	const [loading, setLoading] = useState(false);
-	const [data, setData] = useState(null);
-	const isSm = useMediaQuery("only screen and (max-width : 700px)");
-	const isMd = useMediaQuery(
-		"only screen and (min-width : 700px) and (max-width : 1250px)"
-	);
-	const isLg = useMediaQuery(
-		"only screen and (min-width : 1250px) and (max-width : 1300px)"
-	);
-	const isXl = useMediaQuery("only screen and (min-width : 1201px)");
-	const history = useHistory();
+  const [loading, setLoading] = useState(false);
+  const [data, setData] = useState(null);
+  const isSm = useMediaQuery("only screen and (max-width : 700px)");
+  const isMd = useMediaQuery(
+    "only screen and (min-width : 700px) and (max-width : 1250px)"
+  );
+  const isLg = useMediaQuery(
+    "only screen and (min-width : 1250px) and (max-width : 1300px)"
+  );
+  const isXl = useMediaQuery("only screen and (min-width : 1201px)");
+  const history = useHistory();
 
-	const fetchFavorites = async () => {
-		setLoading(true);
-		try {
-			let response = await axiosInstance.get("/user/favourites");
-			setData(response?.data?.data);
-			// console.log(response.data.data, "heyyyyy");
-		} catch (error) {
-			console.log(error);
-		} finally {
-			setLoading(false);
-		}
-	};
+  const fetchFavorites = async () => {
+    setLoading(true);
+    try {
+      let response = await axiosInstance.get("/user/favourites");
+      setData(response?.data?.data);
+      // console.log(response.data.data, "heyyyyy");
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
+  };
 
-	useMemo(() => {
-		fetchFavorites();
-	}, []);
+  useMemo(() => {
+    fetchFavorites();
+  }, []);
 
-	console.log(data, "DATAAAAAA");
+  console.log(data, "DATAAAAAA");
 
-	const truncateText = (text, maxWords) => {
-		const wordsArray = text.split(" ");
-		if (wordsArray.length > maxWords) {
-			return wordsArray.slice(0, maxWords).join(" ") + "...";
-		} else {
-			return text;
-		}
-	};
+  const truncateText = (text, maxWords) => {
+    const wordsArray = text.split(" ");
+    if (wordsArray.length > maxWords) {
+      return wordsArray.slice(0, maxWords).join(" ") + "...";
+    } else {
+      return text;
+    }
+  };
 
-	return (
-		<>
-			<div class="wrapper">
-				<MyHeader />
-				<Menu />
-				<div
-					style={{
-						marginTop: "3.8rem",
-						width: isSm ? "100%" : "" | isMd ? "80%" : "",
-						marginLeft: isSm ? "" : "17.8rem" | isMd ? "" : "17",
-						display: "flex",
-						justifyContent: "center",
-						alignItems: "center",
-						backgroundColor: "#1d2755"
-					}}
-					class="alert alert-success"
-					role="alert"
-				>
-					<div
-						style={{ textAlign: "left", width: "40%", cursor: "pointer" }}
-					>
-						<i
-							onClick={() => history.push("/scholars")}
-							style={{
-								fontWeight: "bolder",
-								fontSize: "3rem"
-							}}
-							class="fa fa-arrow-circle-left"
-							aria-hidden="true"
-						></i>
-					</div>
+  return (
+    <>
+      <div class="wrapper">
+        <MyHeader />
+        <Menu />
+        <div
+          style={{
+            marginTop: "3.8rem",
+            width: isSm ? "100%" : "" | isMd ? "80%" : "",
+            marginLeft: isSm ? "" : "17.8rem" | isMd ? "" : "17",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            backgroundColor: "#1d2755",
+          }}
+          class="alert alert-success"
+          role="alert"
+        >
+          <div style={{ textAlign: "left", width: "40%", cursor: "pointer" }}>
+            <i
+              onClick={() => history.push("/scholars")}
+              style={{
+                fontWeight: "bolder",
+                fontSize: "3rem",
+              }}
+              class="fa fa-arrow-circle-left"
+              aria-hidden="true"
+            ></i>
+          </div>
 
-					<div style={{ width: "60%", textAlign: "left" }}>
-						<h4
-							style={{
-								fontWeight: "bolder",
-								fontSize: "3.2rem",
-								fontFamily: "Poppins",
-								letterSpacing: "1.6px"
-							}}
-							class="alert-heading"
-						>
-							Saved
-						</h4>
-					</div>
-				</div>
-				{data?.length === 0 ? (
-					<div
-						style={{
-							textAlign: "center",
-							marginTop: "4rem",
-							fontFamily: "Poppins",
-							cursor: "pointer",
-							marginLeft: isSm ? "" : "6rem"
-						}}
-					>
-						<h4
-							style={{
-								fontSize: "18px",
-								letterSpacing: "1.6px",
-								fontWeight: "400",
-								lineHeight: "27px"
-							}}
-						>
-							Start saving scholarships now!
-						</h4>
-						<p
-							onClick={() => history.push("/scholars")}
-							style={{
-								fontSize: "14px",
-								letterSpacing: "1.6px",
-								fontWeight: 400,
-								lineHeight: "20px",
-								color: "rgb(74,74,74)"
-							}}
-						>
-							Back to matches
-						</p>
-					</div>
-				) : (
-					data?.map((dta) => (
-						<LargeCardFavourite
-							title={dta.favourite.title}
-							formatDate={dta.favourite.deadline}
-							subText={truncateText(dta.favourite.about, 6)}
-							award={dta.favourite.award}
-							link={dta.favourite.link}
-						/>
-					))
-				)}
-				<Footer />
-			</div>
+          <div style={{ width: "60%", textAlign: "left" }}>
+            <h4
+              style={{
+                fontWeight: "bolder",
+                fontSize: "3.2rem",
+                fontFamily: "Poppins",
+                letterSpacing: "1.6px",
+              }}
+              class="alert-heading"
+            >
+              Saved
+            </h4>
+          </div>
+        </div>
+        {data?.length === 0 ? (
+          <div
+            style={{
+              textAlign: "center",
+              marginTop: "4rem",
+              fontFamily: "Poppins",
+              cursor: "pointer",
+              marginLeft: isSm ? "" : "6rem",
+            }}
+          >
+            <h4
+              style={{
+                fontSize: "18px",
+                letterSpacing: "1.6px",
+                fontWeight: "400",
+                lineHeight: "27px",
+              }}
+            >
+              Start saving scholarships now!
+            </h4>
+            <p
+              onClick={() => history.push("/scholars")}
+              style={{
+                fontSize: "14px",
+                letterSpacing: "1.6px",
+                fontWeight: 400,
+                lineHeight: "20px",
+                color: "rgb(74,74,74)",
+              }}
+            >
+              Back to matches
+            </p>
+          </div>
+        ) : (
+          data?.map((dta) => (
+            <LargeCardFavourite
+              title={dta?.favourite.title}
+              formatDate={dta?.favourite.deadline}
+              subText={truncateText(dta?.favourite.about, 6)}
+              award={dta?.favourite.award}
+              link={dta?.favourite.link}
+            />
+          ))
+        )}
+        <Footer />
+      </div>
 
-			{/* <Dashboard /> */}
-		</>
-	);
+      {/* <Dashboard /> */}
+    </>
+  );
 };
 
 export default Favorite;
