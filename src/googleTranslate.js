2768e1425ca0a793fbfc3e4121f97236e350a3a6
Hall-e3 <hallenochasanda@gmail.com>
2023-09-27 12:06:24 +0300

    new changes on google translate component

--- src/googleTranslate.js
+++
@@ -1,9 +0,0 @@
-export const googleTranslateElementInit = () => {
-  new window.google.translate.TranslateElement(
-    {
-      pageLanguage: "en",
-      autoDisplay: false,
-    },
-    "google_translate_element"
-  );
-};
