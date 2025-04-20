d8d3ada62608a532f36220a590786e11a12de900
fynn emmanuel junior <fynn.emmanuel.junior@gmail.com>
2025-04-20 20:23:34 +0000

    removed tawk

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "https://api.kodoscholarship.com/api/v1";
+export const BASE_URL = "https://kodo-backend.kodoscholarship.com/api/v1";
 export const config = {
 	headers: {
 		"Content-Type": "application/json",
